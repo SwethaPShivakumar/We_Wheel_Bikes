4f2a1203b91cb7b938a90f713704a41ff0dc4030
Sally868 <kimdungqcd1@gmail.com>
2021-11-25 11:55:04 +1030

    updating booking controller 1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,7 +17,6 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to bookings_path
     else
-      puts @bike.errors.full_messages
       render :new
     end
   end
